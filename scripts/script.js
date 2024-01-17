f8e021d066c91a3b915dc20ada2165a39b3f0350
Роман Слюсар <rosomaha209@gmail.com>
2024-01-18 00:41:04 +0200

    add style

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const listItems = document.querySelectorAll(".course__list-item");
     const hoverImage = document.getElementById("hoverImg");
     hoverImage.style.display = "none";
-    const offset = 500;
+    
     listItems.forEach((item) => {
         item.addEventListener("mouseover", function (event) {
             const imagePath = item.getAttribute("data-image");
             hoverImage.src = imagePath;
 
-            // Отримання координат миші
+            
             const mouseX = event.clientX;
             const mouseY = event.clientY;
 
-            // Встановлення положення зображення відносно миші
+            
             hoverImage.style.left = mouseX  + "px";
             hoverImage.style.top = mouseY  + "px";
+            
 
             hoverImage.style.display = "block";
+            
         });
 
         item.addEventListener("mouseout", function () {
@@ -25,11 +27,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.addEventListener("mousemove", function (event) {
-        // Оновлення положення зображення при русі миші
+        
         const mouseX = event.clientX;
         const mouseY = event.clientY;
 
         hoverImage.style.left = mouseX + "px";
-            hoverImage.style.top = mouseY  + "px";
+        hoverImage.style.top = mouseY  + "px";
+        
+    });
+
+    document.addEventListener("click",function (){
+        hoverImage.style.left = 100 + "px";
+        hoverImage.style.top = 100  + "px";
+        
     });
 });
