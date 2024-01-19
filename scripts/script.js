611c110b5eff45f7e57208463542a21be6e60533
Роман Слюсар <rosomaha209@gmail.com>
2024-01-19 20:20:00 +0200

    Add certificate

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,35 @@
+
+function openCertificate(imageSrc) {
+    console.log("openCertificate")
+    var modal = document.createElement("div");
+    modal.style.position = "fixed";
+    modal.style.top = "0";
+    modal.style.left = "0";
+    modal.style.width = "100%";
+    modal.style.height = "100%";
+    modal.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    modal.style.display = "flex";
+    modal.style.alignItems = "center";
+    modal.style.justifyContent = "center";
+    modal.style.zIndex = 999;
+
+    var certificateImage = document.createElement("img");
+    certificateImage.src = imageSrc;
+    certificateImage.alt = "Certificate";
+    certificateImage.style.maxWidth = "100%";
+    certificateImage.style.maxHeight = "100%";
+
+    modal.appendChild(certificateImage);
+
+    document.body.appendChild(modal);
+
+    modal.onclick = function () {
+        modal.style.display = "none";
+    };
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const listItems = document.querySelectorAll(".course__list-item");
     const hoverImage = document.getElementById("hoverImg");
@@ -47,7 +79,6 @@ document.addEventListener("DOMContentLoaded", function () {
     tableContainer.style.maxHeight = (tableContainer.style.maxHeight === '0px') ? tableContainer.scrollHeight + 'px' : '0';
    });
     
-  
   
   
    
