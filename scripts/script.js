2fe21562a6b2b02166b32e606376270a9f680125
Роман Слюсар <rosomaha209@gmail.com>
2024-01-20 00:08:15 +0200

    ready certeficate page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 
 function openCertificate(imageSrc) {
-    console.log("openCertificate")
     var modal = document.createElement("div");
     modal.style.position = "fixed";
     modal.style.top = "0";
@@ -28,5 +27,79 @@ function openCertificate(imageSrc) {
     };
 }
 
+var arrowName = document.getElementsByClassName("sort__arrow-name")[0];
+var arrowDate = document.getElementsByClassName("sort__arrow-date")[0];
+
+function sortTableByDate() {
+    var table = document.getElementById("courseTable");
+    var rows = table.getElementsByTagName("tr");
+    var data = [];
+
+    // Збираємо дані з таблиці в масив
+    for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        var courseName = cells[0].innerText.trim();
+        var completionDate = cells[1].innerText.trim();
+
+        // Змінюємо формат дати на "MM/DD/YYYY"
+        var parts = completionDate.split(".");
+        var formattedDate = parts[1] + "/" + parts[0] + "/" + parts[2];
+
+        var courseData = { courseName: courseName, completionDate: new Date(formattedDate) };
+        data.push(courseData);
+    }
+
+    // Сортуємо масив за датою
+    data.sort(function(a, b) {
+        return a.completionDate - b.completionDate;
+    });
+
+    // Перезаписуємо дані в таблицю
+    for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        cells[0].innerText = data[i - 1].courseName;
+        cells[1].innerText = data[i - 1].completionDate.toLocaleDateString(); // Повертаємо у форматі тексту
+    }
+    // Задаємо стиль для стрілки сортування за датою
+    arrowDate.style.color = "aqua";
+    arrowDate.style.textShadow = "1px 0px 10px aqua";
+    // Скидаємо колір для стрілки сортування за іменем
+    arrowName.style.color = "";
+    arrowName.style.textShadow = "";
+    
+}
+function sortTableByName() {
+    var table = document.getElementById("courseTable");
+    var rows = table.getElementsByTagName("tr");
+    var data = [];
+
+    // Збираємо дані з таблиці в масив
+    for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        var courseName = cells[0].innerText.trim();
+        var completionDate = cells[1].innerText.trim();
+
+        var courseData = { courseName: courseName, completionDate: completionDate };
+        data.push(courseData);
+    }
+
+    // Сортуємо масив за іменем курсу
+    data.sort(function(a, b) {
+        return a.courseName.localeCompare(b.courseName);
+    });
+
+    // Перезаписуємо дані в таблицю
+    for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        cells[0].innerText = data[i - 1].courseName;
+        cells[1].innerText = data[i - 1].completionDate;
+    }
+     // Задаємо стиль для стрілки сортування за іменем
+     arrowName.style.color = "aqua";
+     arrowName.style.textShadow = "1px 0px 10px aqua";
+     // Скидаємо колір для стрілки сортування за датою
+     arrowDate.style.color = "";
+     arrowDate.style.textShadow = "";
+}
 
 
