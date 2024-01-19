dbc75b3d38c79344559d90de1805777dda43e7e1
Роман Слюсар <rosomaha209@gmail.com>
2024-01-19 22:46:44 +0200

    add new page

--- scripts/script.js
+++ scripts/script.js
@@ -16,8 +16,8 @@ function openCertificate(imageSrc) {
     var certificateImage = document.createElement("img");
     certificateImage.src = imageSrc;
     certificateImage.alt = "Certificate";
-    certificateImage.style.maxWidth = "100%";
-    certificateImage.style.maxHeight = "100%";
+    certificateImage.style.maxWidth = "765px";
+    certificateImage.style.maxHeight = "560px";
 
     modal.appendChild(certificateImage);
 
@@ -30,56 +30,3 @@ function openCertificate(imageSrc) {
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const listItems = document.querySelectorAll(".course__list-item");
-    const hoverImage = document.getElementById("hoverImg");
-    hoverImage.style.display = "none";
-   
-    
-    listItems.forEach((item) => {
-        item.addEventListener("mouseover", function (event) {
-            const imagePath = item.getAttribute("data-image");
-            hoverImage.src = imagePath;
-
-            
-            const mouseX = event.clientX;
-            const mouseY = event.clientY;
-
-            
-            hoverImage.style.left = mouseX  + "px";
-            hoverImage.style.top = mouseY  + "px";
-            
-
-            hoverImage.style.display = "block";
-
-            
-            
-        });
-
-        item.addEventListener("mouseout", function () {
-            hoverImage.style.display = "none";
-        });
-    });
-
-    document.addEventListener("mousemove", function (event) {
-        
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-
-        hoverImage.style.left = mouseX + "px";
-        hoverImage.style.top = mouseY  + "px";
-        
-    });
-   // Отримання елементів DOM
-   var dropdownBtn = document.getElementById('dropdown-btn');
-   var tableContainer = document.querySelector('.table-container');
-
-   // Додавання обробника подій для кнопки
-   dropdownBtn.addEventListener('click', function() {
-    tableContainer.style.maxHeight = (tableContainer.style.maxHeight === '0px') ? tableContainer.scrollHeight + 'px' : '0';
-   });
-    
-  
-  
-   
-});
