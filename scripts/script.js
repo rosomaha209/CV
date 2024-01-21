b78819f838f7a9529c0dfcad3efa440c3c325fe2
Роман Слюсар <rosomaha209@gmail.com>
2024-01-21 17:27:43 +0200

    add new certificate and update table sort

--- scripts/script.js
+++ scripts/script.js
@@ -29,12 +29,50 @@ function openCertificate(imageSrc) {
 
 var arrowName = document.getElementsByClassName("sort__arrow-name")[0];
 var arrowDate = document.getElementsByClassName("sort__arrow-date")[0];
+var sortOrderName = "asc";
+var sortOrderDate = "asc";
+
+function sortTableByName() {
+    var table = document.getElementById("courseTable");
+    var rows = table.getElementsByTagName("tr");
+    var data = [];
+    
+    sortOrderName = (sortOrderName === "asc") ? "desc" : "asc";
+
+    for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        var courseName = cells[0].innerText.trim();
+        var completionDate = cells[1].innerText.trim();
+        var onclickData = cells[0].getAttribute("onclick");
+
+        var courseData = { courseName: courseName, completionDate: completionDate, onclick: onclickData };
+        data.push(courseData);
+    }
+
+    data.sort(function (a, b) {
+        var order = (sortOrderName === "asc") ? 1 : -1;
+        return order * a.courseName.localeCompare(b.courseName);
+    });
+
+    for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        cells[0].innerText = data[i - 1].courseName;
+        cells[1].innerText = data[i - 1].completionDate;
+        cells[0].setAttribute("onclick", data[i - 1].onclick);
+    }
+
+    updateSortIndicator(arrowName, sortOrderName);
+    arrowDate.style.color = "#f1f2f2";
+    arrowDate.style.textShadow = "none";
+}
 
 function sortTableByDate() {
     var table = document.getElementById("courseTable");
     var rows = table.getElementsByTagName("tr");
     var data = [];
-
+    
+    
+    sortOrderDate = (sortOrderDate === "asc") ? "desc" : "asc";
 
     for (var i = 1; i < rows.length; i++) {
         var cells = rows[i].getElementsByTagName("td");
@@ -44,62 +82,41 @@ function sortTableByDate() {
         var parts = completionDate.split(".");
         var formattedDate = parts[1] + "/" + parts[0] + "/" + parts[2];
 
-        var courseData = { courseName: courseName, completionDate: new Date(formattedDate) };
+        var courseData = { courseName: courseName, completionDate: new Date(formattedDate), onclick: cells[0].getAttribute("onclick") };
         data.push(courseData);
     }
 
-
     data.sort(function (a, b) {
-        return a.completionDate - b.completionDate;
+        var order = (sortOrderDate === "asc") ? 1 : -1;
+        return order * (a.completionDate - b.completionDate);
     });
-
 
     for (var i = 1; i < rows.length; i++) {
         var cells = rows[i].getElementsByTagName("td");
         cells[0].innerText = data[i - 1].courseName;
-        cells[1].innerText = data[i - 1].completionDate.toLocaleDateString(); // Повертаємо у форматі тексту
+        cells[1].innerText = data[i - 1].completionDate.toLocaleDateString();
+        cells[0].setAttribute("onclick", data[i - 1].onclick);
     }
-
-    arrowDate.style.color = "aqua";
-    arrowDate.style.textShadow = "1px 0px 10px aqua";
-
-    arrowName.style.color = "";
-    arrowName.style.textShadow = "";
-
+ 
+    updateSortIndicator(arrowDate, sortOrderDate);
+ 
+    arrowName.style.color = "#f1f2f2";
+    arrowName.style.textShadow = "none";
 }
-function sortTableByName() {
-    var table = document.getElementById("courseTable");
-    var rows = table.getElementsByTagName("tr");
-    var data = [];
-
-
-    for (var i = 1; i < rows.length; i++) {
-        var cells = rows[i].getElementsByTagName("td");
-        var courseName = cells[0].innerText.trim();
-        var completionDate = cells[1].innerText.trim();
-
-        var courseData = { courseName: courseName, completionDate: completionDate };
-        data.push(courseData);
+function updateSortIndicator(arrowElement, sortOrder) {
+   
+    if (sortOrder === "asc") {
+        arrowElement.innerHTML = "↓";
+    } else {
+        arrowElement.innerHTML = "↑";
     }
 
-
-    data.sort(function (a, b) {
-        return a.courseName.localeCompare(b.courseName);
-    });
-
-
-    for (var i = 1; i < rows.length; i++) {
-        var cells = rows[i].getElementsByTagName("td");
-        cells[0].innerText = data[i - 1].courseName;
-        cells[1].innerText = data[i - 1].completionDate;
-    }
-
-    arrowName.style.color = "aqua";
-    arrowName.style.textShadow = "1px 0px 10px aqua";
-
-    arrowDate.style.color = "";
-    arrowDate.style.textShadow = "";
+    
+    arrowElement.style.color = "aqua";
+    arrowElement.style.textShadow = "1px 0px 10px aqua";
+    
 }
+
 window.onload = function() {
     if (window.innerWidth <= 768) {
     
